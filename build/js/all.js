48d882e588351fd94ccf6d4324db58ddec51fcc1
GrayD77 <siarhei.drahichynski@gmail.com>
2017-08-24 23:25:17 +0300

    task-2 solved

--- build/js/all.js
+++ build/js/all.js
@@ -67,7 +67,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 ;(function () {
 
-	function parseArgs() {
+	// берет window.location.search и отдает объект с переданными параметрами
+	function parseSearchString() {
 		var args = window.location.search.slice(1).split('&');
 		var argsParsed = args.reduce(function (summ, current, index, array) {
 
@@ -84,20 +85,133 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 		return argsParsed;
 	}
 
-	function fillTheForm(form, values) {
-		// form- DOM object
-		// values -  { key: value, ... }
-		for (var key in values) {
+	// 
+	function formToSearchString(form, filter) {
+		var resArray = []; // response Array
 
-			form.elements[key].value = values[key];
+		if (!form || form.nodeName !== 'FORM') return;
+
+		for (var i = 0, l = form.elements.length; i < l; i++) {
+			var name = form.elements[i].name;
+			var type = void 0;
+
+			if (name === '' || filter.indexOf(name) !== -1) continue;
+
+			type = form.querySelector('[name="' + name + '"]').type;
+			filter.push(name);
+
+			switch (type) {
+
+				case 'radio':
+				case 'checkbox':
+				case 'select-multiple':
+					{
+						var selector = void 0;
+						var checkedPropName = void 0;
+						var inputGroup = void 0;
+						var values = [];
+
+						if (type === 'select-multiple') {
+							selector = '[name="' + name + '"] option';
+							checkedPropName = 'selected';
+						} else {
+							selector = '[name="' + name + '"]';
+							checkedPropName = 'checked';
+						}
+
+						inputGroup = form.querySelectorAll(selector);
+
+						for (var _i = 0, _l = inputGroup.length; _i < _l; _i++) {
+							var currentElement = inputGroup[_i];
+
+							if (currentElement[checkedPropName]) {
+								values.push(currentElement.value);
+							}
+						}
+
+						resArray.push(name + '=' + values.join(','));
+
+						break;
+					}
+
+				default:
+					{
+						resArray.push(name + '=' + form.elements[i].value);
+					}
+			}
 		}
 
-		// console.log(form.elements['size'].value = 'L');
+		return resArray.join('&');
 	}
 
-	var args = parseArgs();
+	// заполняет форму полями объекта 'values'
+	function fillTheForm(form, values) {
+		// form - form DOM element
+		// values -  { key: value, ... }
+
+		for (var key in values) {
+			setValue(form, key, values[key]);
+		}
+	}
+
+	// заполняет поле 'key' формы значением 'value'
+	function setValue(form, key, value) {
+		var type = form.querySelector('[name="' + key + '"]').type;
+
+		switch (type) {
+
+			// checkbox and multiple select
+			case 'checkbox':
+			case 'select-multiple':
+				{
+					var selector = void 0;
+					var checkedPropName = void 0;
+					var inputGroup = void 0;
+
+					if (type === 'checkbox') {
+						selector = '[name="' + key + '"]';
+						checkedPropName = 'checked';
+					} else {
+						selector = '[name="' + key + '"] option';
+						checkedPropName = 'selected';
+					}
+
+					inputGroup = form.querySelectorAll(selector);
+
+					if (Object.prototype.toString.call(value) === '[object String]') {
+						value = [value];
+						// сейчас value - всегда массив
+					}
+
+					for (var i = 0, l = inputGroup.length; i < l; i++) {
+						var currentElement = inputGroup[i];
+						var currentElementValue = currentElement.value;
+						var checked = value.indexOf(currentElementValue) !== -1;
+
+						currentElement[checkedPropName] = checked;
+					}
+
+					break;
+				}
+
+			// text, password, email, textarea, radio
+			default:
+				form.elements[key].value = value;
+		}
+	}
+
+	var args = parseSearchString();
 	var form = document.querySelector('.query-params');
 
 	fillTheForm(form, args);
+
+	form.addEventListener('change', function () {
+		console.log(window.location.href.split('?')[0] + '?' + formToSearchString(this, ['on-sale']));
+	}, false);
+
+	form.querySelector('[name="on-sale"]').addEventListener('change', function (e) {
+		e.stopPropagation();
+		return false;
+	}, false);
 })();
 //# sourceMappingURL=all.js.map
--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,6 +1,7 @@
 ;(function(){
 
-	function parseArgs() {
+	// берет window.location.search и отдает объект с переданными параметрами
+	function parseSearchString() {
 		let args = window.location.search.slice(1).split('&');
 		let argsParsed = args.reduce( (summ, current, index, array) => {
 
@@ -18,24 +19,138 @@
 	}
 
 
-	function fillTheForm(form, values) {
-		// form- DOM object
-		// values -  { key: value, ... }
-		for (let key in values) {
-			
-			form.elements[key].value = values[key];
+	// 
+	function formToSearchString(form, filter) {
+		let resArray = []; // response Array
 
+		if (!form || form.nodeName !== 'FORM') return;
+
+		for (let i = 0, l = form.elements.length; i < l; i++) {
+			let name = form.elements[i].name;
+			let type;
+
+			if (name === '' || filter.indexOf(name) !== -1) continue;
+
+				type = form.querySelector(`[name="${name}"]`).type;
+				filter.push(name);
+
+				switch (type) {
+
+					case 'radio':
+					case 'checkbox':
+					case 'select-multiple': {
+						let selector;
+						let checkedPropName;
+						let inputGroup;
+						let values = [];
+
+						if (type === 'select-multiple') {
+							selector = `[name="${name}"] option`;
+							checkedPropName = 'selected';
+						} else {
+							selector = `[name="${name}"]`;
+							checkedPropName = 'checked';
+						}
+
+						inputGroup = form.querySelectorAll(selector);
+
+						for (let i = 0, l = inputGroup.length; i < l; i++) {
+							let currentElement = inputGroup[i];
+
+							if (currentElement[checkedPropName]) {
+								values.push(currentElement.value);
+							}
+						}
+
+						resArray.push(`${name}=${values.join(',')}`);
+
+						break;
+					}
+
+					default: {
+						resArray.push(`${name}=${form.elements[i].value}`);
+					}
+				}
 		}
 
-		// console.log(form.elements['size'].value = 'L');
+		return resArray.join('&');
+
+	}
+
+	// заполняет форму полями объекта 'values'
+	function fillTheForm(form, values) {
+		// form - form DOM element
+		// values -  { key: value, ... }
+
+		for (let key in values) {
+			setValue(form, key, values[key])
+		}
+
 	}
 
 
-	let args = parseArgs();
+	// заполняет поле 'key' формы значением 'value'
+	function setValue(form, key, value){
+		let type = form.querySelector(`[name="${key}"]`).type;
+
+		switch (type) {
+
+			// checkbox and multiple select
+			case 'checkbox':
+			case 'select-multiple': {
+				let selector;
+				let checkedPropName;
+				let inputGroup;
+
+				if (type === 'checkbox') {
+					selector = `[name="${key}"]`;
+					checkedPropName = 'checked';
+				} else {
+					selector = `[name="${key}"] option`;
+					checkedPropName = 'selected';
+				}
+
+				inputGroup = form.querySelectorAll(selector);
+
+				if (Object.prototype.toString.call(value) === '[object String]') {
+					value = [value];
+					// сейчас value - всегда массив
+				}
+
+				for (let i = 0, l = inputGroup.length; i < l; i++) {
+					let currentElement = inputGroup[i];
+					let currentElementValue = currentElement.value;
+					let checked = value.indexOf(currentElementValue) !== -1;
+
+					currentElement[checkedPropName] = checked;
+				}
+
+				break;
+			}
+
+			// text, password, email, textarea, radio
+			default:
+				form.elements[key].value = value;
+		}
+	}
+
+
+
+
+	let args = parseSearchString();
 	let form = document.querySelector('.query-params');
 
 	fillTheForm(form, args);
 
 
+
+	form.addEventListener('change', function() {
+		console.log(`${window.location.href.split('?')[0]}?${formToSearchString(this, ['on-sale'])}`);
+	}, false);
+
+	form.querySelector('[name="on-sale"]').addEventListener('change', function(e) {
+			e.stopPropagation();
+			return false;
+	}, false);
 
 }());
